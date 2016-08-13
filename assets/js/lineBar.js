41860c92f51bd6f73267dc480d46ddbb0c918ef6
Victoria Ward <vcward@hawaii.rr.com>
2016-08-12 18:51:54 -1000

    create line-bar chart combination & list of categories

---
+++ assets/js/lineBar.js
@@ -0,0 +1,59 @@
+d3.json('http://localhost:8080/v1/series/observations?id=1', function(data) {
+   var level = data.transformationResults[0].observations;
+   var perc = data.transformationResults[1].observations;
+   var levelValues = [];
+   var percValues = [];
+   var dates = [];
+
+   for(i = 0; i < level.length; i++) {
+      levelValues.push(+level[i].value);
+      dates.push(level[i].date);
+      percValues.push(+perc[i].value);
+   }
+
+   /* console.log('Level data: ');
+   console.log(level);
+   console.log('Percent change data: ');
+   console.log(perc);
+   console.log('Level values: ');
+   console.log(levelValues);
+   console.log('Percent Values: ');
+   console.log(percValues);
+   console.log('Observation dates: ');
+   console.log(dates); */
+
+   new Highcharts.chart({
+      chart: {
+         renderTo: lineBar
+      },
+      xAxis: {
+         categories: dates.reverse()
+      },
+      yAxis: [{
+         Labels: {
+            format: '{value}'
+         },
+         title: {
+            text: 'Rate of Change (%)'
+         }
+      }, {
+         title: {
+            text: 'Total Visitor Arrivals'
+         },
+         labels: {
+            format: '{value}'
+         },
+         opposite: true
+      }],
+      series: [{
+         name: 'Rate of Change (%)',
+         type: 'column',
+         data: percValues.reverse()
+      }, {
+         name: 'Total Visitor Arrivals',
+         type: 'spline',
+         yAxis: 1,
+         data: levelValues.reverse()
+      }]
+   });
+});
